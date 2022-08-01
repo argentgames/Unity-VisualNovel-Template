87dcccffba1aeef535f66b304c33fa4ec3b1a756
Nikita Haduong <nikita.haduong@gmail.com>
2022-07-31 21:54:37 -0700

    fix imagemanager bg

--- Packages/VisualNovelTemplate/Runtime/Scripts/ScriptableObjectTemplates/Images/Shot.cs
+++ Packages/VisualNovelTemplate/Runtime/Scripts/ScriptableObjectTemplates/Images/Shot.cs
@@ -2,8 +2,11 @@ using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 
+
 namespace com.argentgames.visualnoveltemplate
 {
+    [CreateAssetMenu(fileName = "Shot", menuName = "Argent Games/Visual Novel Template/ScriptableObjects/Shot")]
+
     /// <summary>
     /// Cinematic background with default location/rotation. 
     /// Used in inkscript to show a new background.
@@ -12,10 +15,20 @@ namespace com.argentgames.visualnoveltemplate
     {
 
         public string bgName;
-        public AssetReference bgPrefab;
+        public bool UseAddressables = false;
+        [HideIf("@this.UseAddressables")]
+        [DisableIf("@this.bgAssetReference.RuntimeKeyIsValid()")]
+        public GameObject bgPrefab;
+        [HideIf("@this.UseAddressables == false")]
+        [DisableIf("@this.bgPrefab != null")]
+        public AssetReference bgAssetReference;
         public Vector3 position;
         public Vector3 rotation;
+        /// <summary>
+        /// Orthographic camera size (zoom amount)
+        /// </summary>
         public float size;
+        public ImageLayer imageLayer = ImageLayer.CurrentBackground;
 
         // TODO: maybe we want a shot that has a custom animation, but we want
         // to call the custom animatino at a specified time in the script, rather
