778f0c47bd84af8832877c491389c207df634797
Nikita Haduong <nikita.haduong@gmail.com>
2022-09-22 09:05:11 -0700

    add manual delay to wait for things to load up; in future should change to checking the Managers AllReady flag

--- Runtime/Scripts/SceneBrains/Preload.cs
+++ Runtime/Scripts/SceneBrains/Preload.cs
@@ -2,16 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using NaughtyAttributes;
+using Cysharp.Threading.Tasks;
 namespace com.argentgames.visualnoveltemplate
 {
     public class Preload : MonoBehaviour
     {
+        [SerializeField]
+        float delayBeforeLoadScene = .2f;
         [Scene]
         public string sceneToLoad = "MainMenu";
         // Start is called before the first frame update
-        void Start()
+        void Awake()
         {
+            SceneTransitionManager.Instance.FadeToBlack(0);
+        }
+        async UniTaskVoid Start()
+        {
+            await UniTask.Delay(System.TimeSpan.FromSeconds(delayBeforeLoadScene));
             SceneTransitionManager.Instance.LoadScene(sceneToLoad,UnityEngine.SceneManagement.LoadSceneMode.Single);
+            SceneTransitionManager.Instance.FadeIn(0);
         }
 
         // Update is called once per frame
