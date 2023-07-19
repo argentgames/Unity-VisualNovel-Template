5334304b9cef71f47e93f8fc2ff70add9744847b
Argent Games <contact@argentgames.co>
2023-07-19 14:18:19 -0700

    use single mbp to update shader properties

--- Runtime/Scripts/GUI/TogglePhotoshopLayerStylesInShader.cs
+++ Runtime/Scripts/GUI/TogglePhotoshopLayerStylesInShader.cs
@@ -1,42 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+
 namespace com.argentgames.visualnoveltemplate
 {
     public class TogglePhotoshopLayerStylesInShader : MonoBehaviour
     {
         [SerializeField]
         bool multiply = false;
-        
-SpriteRenderer image;
+
+        SpriteRenderer image;
+        MaterialPropertyBlockUtilities materialPropertyBlockUtilities;
         private MaterialPropertyBlock _propBlock;
-        
+
         void Awake()
         {
+            materialPropertyBlockUtilities = GetComponent<MaterialPropertyBlockUtilities>();
             image = GetComponent<SpriteRenderer>();
             _propBlock = new MaterialPropertyBlock();
             SetMultiply(multiply);
         }
+
         [Sirenix.OdinInspector.Button]
         public void SetMultiply(bool val)
         {
             multiply = val;
 
-             if (multiply)
+            if (multiply)
             {
-                if (_propBlock == null)
-                {
-                    _propBlock = new MaterialPropertyBlock();
-                    Debug.LogWarningFormat("have to make new propblock for toggle photoshop layer style {0}",gameObject.name);
-                }
-                if (image == null)
-                {
-                    image = GetComponent<SpriteRenderer>();
-                    Debug.LogWarningFormat("have to get image sr in toggle photoshop layer for obj {0}",gameObject.name);
-                }
-                image.GetPropertyBlock(_propBlock);
-            _propBlock.SetFloat("_NeedsMultiply",1);
-            image.SetPropertyBlock(_propBlock);
+                materialPropertyBlockUtilities.UpdateProperties(
+                    new Tuple<string, float>("_NeedsMultiply", 1)
+                );
+                // if (_propBlock == null)
+                // {
+                //     _propBlock = new MaterialPropertyBlock();
+                //     Debug.LogWarningFormat(
+                //         "have to make new propblock for toggle photoshop layer style {0}",
+                //         gameObject.name
+                //     );
+                // }
+                // if (image == null)
+                // {
+                //     image = GetComponent<SpriteRenderer>();
+                //     Debug.LogWarningFormat(
+                //         "have to get image sr in toggle photoshop layer for obj {0}",
+                //         gameObject.name
+                //     );
+                // }
+                // image.GetPropertyBlock(_propBlock);
+                // _propBlock.SetFloat("_NeedsMultiply", 1);
+                // image.SetPropertyBlock(_propBlock);
+                Debug.Log("done running multiply");
             }
         }
     }
---
+++ Runtime/Scripts/Utilities/MaterialPropertyBlockUtilities.cs
@@ -0,0 +1,142 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace com.argentgames.visualnoveltemplate
+{
+    public class MaterialPropertyBlockUtilities : MonoBehaviour
+    {
+        SpriteRenderer spriteRenderer;
+        MaterialPropertyBlock propBlock;
+
+        private void Awake()
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+            propBlock = new MaterialPropertyBlock();
+        }
+
+        public void UpdateProperties(Tuple<string, float> prop, int materialIndex = 0)
+        {
+            if (spriteRenderer == null)
+            {
+                spriteRenderer = GetComponent<SpriteRenderer>();
+            }
+            if (propBlock == null)
+            {
+                propBlock = new MaterialPropertyBlock();
+            }
+            spriteRenderer.GetPropertyBlock(propBlock);
+
+            Debug.LogFormat("setting property {0} to val {1}", prop.Item1, prop.Item2);
+            propBlock.SetFloat(prop.Item1, prop.Item2);
+
+            spriteRenderer.SetPropertyBlock(propBlock);
+        }
+
+        public void UpdateProperties(Tuple<string, Texture2D> prop, int materialIndex = 0)
+        {
+            if (spriteRenderer == null)
+            {
+                spriteRenderer = GetComponent<SpriteRenderer>();
+            }
+            if (propBlock == null)
+            {
+                propBlock = new MaterialPropertyBlock();
+            }
+            spriteRenderer.GetPropertyBlock(propBlock);
+
+            Debug.LogFormat("setting property {0} to val {1}", prop.Item1, prop.Item2);
+            propBlock.SetTexture(prop.Item1, prop.Item2);
+
+            spriteRenderer.SetPropertyBlock(propBlock);
+
+            spriteRenderer.GetPropertyBlock(propBlock);
+            Debug.LogFormat(
+                "after setting property for go {1} with parent {2}, we have current val {0}",
+                propBlock.GetTexture(prop.Item1).name,
+                this.gameObject.name,
+                this.gameObject.transform.parent.name
+            );
+        }
+
+        public void UpdateProperties(Tuple<string, Color> prop, int materialIndex = 0)
+        {
+            if (spriteRenderer == null)
+            {
+                spriteRenderer = GetComponent<SpriteRenderer>();
+            }
+            if (propBlock == null)
+            {
+                propBlock = new MaterialPropertyBlock();
+            }
+            spriteRenderer.GetPropertyBlock(propBlock);
+
+            Debug.LogFormat("setting property {0} to val {1}", prop.Item1, prop.Item2);
+            propBlock.SetColor(prop.Item1, prop.Item2);
+
+            spriteRenderer.SetPropertyBlock(propBlock);
+        }
+
+        public void UpdateProperties(List<Tuple<string, float>> props, int materialIndex = 0)
+        {
+            if (spriteRenderer == null)
+            {
+                spriteRenderer = GetComponent<SpriteRenderer>();
+            }
+            if (propBlock == null)
+            {
+                propBlock = new MaterialPropertyBlock();
+            }
+            spriteRenderer.GetPropertyBlock(propBlock);
+            foreach (var prop in props)
+            {
+                Debug.LogFormat("setting property {0} to val {1}", prop.Item1, prop.Item2);
+                propBlock.SetFloat(prop.Item1, prop.Item2);
+            }
+
+            spriteRenderer.SetPropertyBlock(propBlock);
+        }
+
+        public void UpdateProperties(List<Tuple<string, Texture2D>> props, int materialIndex = 0)
+        {
+            if (spriteRenderer == null)
+            {
+                spriteRenderer = GetComponent<SpriteRenderer>();
+            }
+            if (propBlock == null)
+            {
+                propBlock = new MaterialPropertyBlock();
+            }
+            spriteRenderer.GetPropertyBlock(propBlock);
+            foreach (var prop in props)
+            {
+                Debug.LogFormat("setting property {0} to val {1}", prop.Item1, prop.Item2);
+                propBlock.SetTexture(prop.Item1, prop.Item2);
+            }
+
+            spriteRenderer.SetPropertyBlock(propBlock);
+        }
+
+        public void UpdateProperties(List<Tuple<string, Color>> props, int materialIndex = 0)
+        {
+            if (spriteRenderer == null)
+            {
+                spriteRenderer = GetComponent<SpriteRenderer>();
+            }
+            if (propBlock == null)
+            {
+                propBlock = new MaterialPropertyBlock();
+            }
+            spriteRenderer.GetPropertyBlock(propBlock);
+            foreach (var prop in props)
+            {
+                Debug.LogFormat("setting property {0} to val {1}", prop.Item1, prop.Item2);
+                propBlock.SetColor(prop.Item1, prop.Item2);
+            }
+
+            spriteRenderer.SetPropertyBlock(propBlock);
+        }
+    }
+}
