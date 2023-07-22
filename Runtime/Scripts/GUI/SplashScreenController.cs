72e7f14a8292ba8175422e05bc39db661d0c4dc9
Nikita Haduong <nikita.haduong@gmail.com>
2023-07-22 10:26:11 -0700

    spawn char from save and set exp before we can see it flicker

--- Runtime/Scripts/GUI/SplashScreenController.cs
+++ Runtime/Scripts/GUI/SplashScreenController.cs
@@ -8,11 +8,19 @@ namespace com.argentgames.visualnoveltemplate
 {
 public class SplashScreenController : MonoBehaviour
 {
-    
-    async UniTaskVoid Start()
+    [SerializeField]
+    AnimateObjectsToggleEnable animateObjectsToggleEnable;
+
+    void Awake()
     {
         SaveLoadManager.Instance.LoadSaveFiles().Forget();
         AudioManager.Instance.GetFMODBuses();
+    }
+    
+    async UniTaskVoid Start()
+    {
+        
+        await animateObjectsToggleEnable.Disable(-1);
         await UniTask.Delay(TimeSpan.FromSeconds(2.5f));
         SceneTransitionManager.Instance.LoadScene("MainMenu",1.5f,1.5f);
         
